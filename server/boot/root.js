a2b16936239b9f48249217240a03b84fd70836b9
Mathew Kleppin <matkle414@gmail.com>
2016-04-20 20:25:33 -0500

    add circle heroku integration and serve webpage

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,6 +1,14 @@
+var path = require('path');
+
 module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
+
+  router.get('/', function(req, res) {
+    var staticPath = path.resolve(__dirname + '/../../client');
+
+    res.sendFile(staticPath + '/index.html');
+  });
+
   server.use(router);
 };
